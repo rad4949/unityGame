8d44ca51b7df3541e09f75b8744efde93baea1ce
Igor Radchuk <igor12radchuk@gmail.com>
2020-11-15 09:04:34 +0200

    Add modern game

---
+++ ApplePicker-master/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    public Text scoreGT;
+    public bool upLevel = false;
+
+    //Шаблон для створення яблука
+    public GameObject applePrefab;
+
+    //Шаблон для створення bomb
+    public GameObject bombPrefab;
+    
+    //Швидкість руху яблуні
+    public float speed = 10f;
+
+    //Відстань на яку повинно змінюватися напрямок руху яблуні
+    public float leftAndRightEnge = 25f;
+
+    //Імовірність случайної зміни напряку руху
+    public float changeToChangeDirections = 0.05f;
+
+    //Частота створення екземплярів яблук
+    public float secondsBetweenAppleDrops = 0.6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Получить компонент Text этого игрового объекта
+        scoreGT = scoreGO.GetComponent<Text>();
+        //Скидання яблука раз в секунду
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position; //позиція яблука рівна позиції яблуні
+        Invoke("DropApple", secondsBetweenAppleDrops);  //кожну секунду буде скидатися нове яблуко
+    }
+
+    void DropBomb()
+    {
+        GameObject bomb = Instantiate<GameObject>(bombPrefab);
+        bomb.transform.position = transform.position; //позиція яблука рівна позиції яблуні
+        Invoke("DropBomb", secondsBetweenAppleDrops);  //кожну секунду буде скидатися нове яблуко
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Преобразовать текст в scoreGT в целое число
+        int score = int.Parse(scoreGT.text);
+        if (score > 2000 && !upLevel)
+        {
+            CancelInvoke("DropApple");
+            Invoke("DropBomb", 2f);
+            upLevel = true;
+        }
+        //Просте переміщення
+        Vector3 pos = transform.position;
+        var deltaTime = Time.deltaTime;
+        pos.x += speed * deltaTime;
+        transform.position = pos;
+
+        //Зміна напрямку
+        if (pos.x < -leftAndRightEnge)
+        {
+            speed = Mathf.Abs(speed); //починаємо рухатися в право
+        }
+        else if (pos.x > leftAndRightEnge)
+        {
+            speed = -Mathf.Abs(speed); //починаємо рухатися в ліво
+        }
+    }
+    private void FixedUpdate() //визываэться только 50 раз в секунду
+    {
+        //Тепер випадкова зміна напрямку привязана до часу,
+        //тому, що виконується в методі FixedUpdate()
+        if (Random.value < changeToChangeDirections)  //починаємо рухатися рандомом, якщо рандом попав
+        {
+            speed *= -1;
+        }
+    }
+}
---
+++ ApplePicker-master/Assets/Scripts/Basket.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+    public float panSpeed = 20f;
+    Vector3 lastMouseCoordinate = Vector3.zero;
+
+    //Відстань на яку повинно змінюватися напрямок руху яблуні
+    public float leftAndRightEnge = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Получить компонент Text этого игрового объекта
+        scoreGT = scoreGO.GetComponent<Text>();
+        // Установить начальное число очков равным 0
+        scoreGT.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Отримати поточні координати мишки на екрані з Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //Переміщаємо корзину вздовж осі X в координату X вказівника миші
+        Vector3 pos = this.transform.position;
+
+        // First we find out how much it has moved, by comparing it with the stored coordinate.
+        Vector3 mouseDelta = mousePos2D - lastMouseCoordinate;
+
+        // Then we check if it has moved to the left.
+        if (mousePos2D != lastMouseCoordinate) // Assuming a negative value is to the left.
+        {
+            //Координати Z камери визначаючий, як далеко в трохмірному просторі
+            //знаходиться вказівник миші
+            mousePos2D.z = -Camera.main.transform.position.z;
+            //Перетворити точку на двухмірній площині єкрану в трохмірні коодинати ігри
+            Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+            pos.x = mousePos3D.x;
+        }
+
+        // Then we store our mousePosition so that we can check it again next frame.
+        lastMouseCoordinate = Input.mousePosition;
+
+        if (Input.GetKey("left"))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (pos.x < -leftAndRightEnge || pos.x > leftAndRightEnge)
+        {
+            pos = this.transform.position;
+        }
+
+        this.transform.position = pos;
+    }
+
+
+    //Визивається всякий раз коли який інший обєкт стикається із корзиною
+    private void OnCollisionEnter(Collision coll)
+    {
+        //Пошук яблука, яке попало в корзину
+        GameObject collidedWidth = coll.gameObject;  //Обєкт який стикнувся із корзиною
+        if (collidedWidth.tag == "Apple")
+        {
+            Destroy(collidedWidth); //Якщо облуко то видаємо його
+            
+            // Преобразовать текст в scoreGT в целое число
+            int score = int.Parse(scoreGT.text);
+            
+            // Добавить очки за пойманное яблоко
+            score += 100;
+            // Преобразовать число очков обратно в строку и вывести ее на экран
+            scoreGT.text = score.ToString();
+        }
+    }
+}
